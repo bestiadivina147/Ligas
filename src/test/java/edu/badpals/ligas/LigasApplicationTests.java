641d46af89d55fe104ade5a2727331bbfa7f2fb7
bestiadivina147 <diegopc147@gmail.com>
2025-06-13 18:35:05 +0200

    Initial commit

---
+++ src/test/java/edu/badpals/ligas/LigasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.badpals.ligas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
